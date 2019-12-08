0a4f12635048bd175097464ea0739e55c321e456
fengzhenquan <616447247@qq.com>
2019-12-08 11:58:37 +0800

    启项

---
+++ koa-graphql-typescript-master/src/database/conectDB.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata'
+import { createConnection } from "typeorm";
+import { MySqlConf, MongoConf } from '../../conf/db.conf'
+import { Entities } from '../entities/mysql'
+import { MongoEntities } from '../entities/mongo'
+
+const _PROD_ = process.env.NODE_ENV === 'production'
+
+const connectDB = (): void => {
+  createConnection({
+    type     : 'mysql',
+    host     : MySqlConf.host,
+    port     : MySqlConf.port,
+    username : MySqlConf.username,
+    password : MySqlConf.password,
+    database : MySqlConf.database,
+    entities : Entities,
+    logging  : _PROD_ ? false : true,
+    // logger   : 'simple-console'
+  }).then((connect) => {
+    console.log('mysql connect success!')
+  }).catch((err) => {
+    console.log('mysql connect fail!', err)
+  })  
+}
+
+const connectMongo = (): void => {
+  createConnection({
+    name     : 'mongo',
+    type     : 'mongodb',
+    host     : MongoConf.host,
+    port     : MongoConf.port,
+    // username : MongoConf.username,
+    // password : MongoConf.password,
+    database : MongoConf.database,
+    entities : MongoEntities,
+    logging  : _PROD_ ? false : true,
+  }).then((connect) => {
+    console.log('mongo connect success!')
+  }).catch((err) => {
+    console.log('mongo connect fail!', err)
+  })  
+}
+
+export {
+  connectDB,
+  connectMongo
+}
